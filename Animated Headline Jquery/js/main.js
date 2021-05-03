19fdb7c05e129c371af6d28c7ea4dae0ef1ebc20
We Coders <74466828+wecoders01@users.noreply.github.com>
2021-05-03 13:24:29 +0600

    Add files via upload

---
+++ Animated Headline Jquery/js/main.js
@@ -0,0 +1,35 @@
+(function ($) {
+    "use strict";
+    
+    $('.selector').animatedHeadline({
+        animationType: 'rotate-1'
+    });
+    $('.selector2').animatedHeadline({
+        animationType: 'rotate-2'
+    });
+    $('.selector3').animatedHeadline({
+        animationType: 'rotate-3'
+    });
+    $('.selector4').animatedHeadline({
+        animationType: 'type'
+    });
+    $('.selector5').animatedHeadline({
+        animationType: 'loading-bar'
+    });
+    $('.selector6').animatedHeadline({
+        animationType: 'slide'
+    });
+    $('.selector7').animatedHeadline({
+        animationType: 'clip'
+    });
+    $('.selector8').animatedHeadline({
+        animationType: 'zoom'
+    });
+    $('.selector9').animatedHeadline({
+        animationType: 'scale'
+    });
+    $('.selector10').animatedHeadline({
+        animationType: 'push'
+    });
+
+})(jQuery);
