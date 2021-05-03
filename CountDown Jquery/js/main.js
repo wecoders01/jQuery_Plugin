f093f080f42cc4a462782661809d0aa60b28cc74
We Coders <74466828+wecoders01@users.noreply.github.com>
2021-05-03 13:26:54 +0600

    Add files via upload

---
+++ CountDown Jquery/js/main.js
@@ -0,0 +1,14 @@
+(function ($) {
+    "use strict";
+    // countdown
+	$('[data-countdown]').each(function () {
+		var $this = $(this), finalDate = $(this).data('countdown');
+		$this.countdown(finalDate, function (event) {
+			$this.html(event.strftime('<div class="time-count">%D <span>days</span></div><div class="time-count">%H <span>hour</span></div><div class="time-count">%M <span>minute</span></div><div class="time-count">%S <span>Second</span></div>'));
+		});
+	});
+
+    
+
+
+})(jQuery);
---
+++ Counter Up Jquery/js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+    "use strict";
+    
+    // Start Counter
+    $('.counter').counterUp({
+        delay: 3,
+        time: 2000,
+    });
+    // End Counter
+
+
+})(jQuery);
